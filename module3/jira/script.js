f16d77709c7abf734a61fe15d0e96de789dab7fa
Shreya0141 <shreyayadav9151@gmail.com>
2022-06-02 14:29:58 +0530

    color change done

--- module3/jira/script.js
+++ module3/jira/script.js
@@ -3,8 +3,13 @@ let modalCont = document.querySelector(".modal-cont");
 let taskAreaCont = document.querySelector(".textarea-cont");
 let mainCont = document.querySelector(".main-cont");
 let allPriorityColors = document.querySelectorAll(".priority-color");
+let removeBtn = document.querySelector(".remove-btn");
+let colors = ["lightpink","blue","green","black"];
+let removemodal = false;
 let addmodal = true;
-let modalPriorityColor = 'black';
+let modalPriorityColor = colors[colors.length-1];
+
+let ticketArr = [];
 
 addbtn.addEventListener("click",function(){
     if(addmodal){
@@ -36,6 +41,16 @@ modalCont.addEventListener("keydown",function(e){
     }
 })
 
+removeBtn.addEventListener("click",function(){
+    if(removemodal){
+        removeBtn.style.color = "black";
+    }
+    else{
+        removeBtn.style.color = "red";
+    }
+    removemodal = !removemodal;
+})
+
 function createTicket(ticketColor,task){
     // <div class="ticket-cont">
             // <div class="ticket-color"></div>
@@ -48,4 +63,31 @@ function createTicket(ticketColor,task){
                              <div class="ticketid">1234</div>
                              <div class="taskarea">${task}</div>`
     mainCont.appendChild(ticketCont);
+    
+
+    //handling remove
+    ticketCont.addEventListener("click",function(){
+        if(removemodal)
+            ticketCont.remove();
+        
+    })
+
+    //handling color
+
+    let ticketColorBand = ticketCont.querySelector(".ticket-color");
+    ticketColorBand.addEventListener("click",function(){
+        let currentTicketColor = ticketColorBand.classList[1];
+        let currentTicketColorIdx = -1;
+        for(let i = 0 ; i <colors.length;i++){
+            if(currentTicketColor == colors[i]){
+                currentTicketColorIdx = i;
+                break;
+            }
+        }
+        let nextColorIdx = (currentTicketColorIdx+1)%colors.length;
+        let nextColor = colors[nextColorIdx];
+        ticketColorBand.classList.remove(currentTicketColor);
+        ticketColorBand.classList.add(nextColor);
+    })
 }
+
